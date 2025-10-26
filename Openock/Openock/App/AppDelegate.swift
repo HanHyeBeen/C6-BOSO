4764f0716b1037415ed5b41358288bfaf398a0c4
JiJoo Maeng <mjiju06@gmail.com>
2025-10-26 19:22:53 +0900

    feat: 윈도우 항상 위로 고정

--- Openock/Openock/App/AppDelegate.swift
+++ Openock/Openock/App/AppDelegate.swift
@@ -13,9 +13,16 @@ final class AppDelegate: NSObject, NSApplicationDelegate {
     NSWindow.allowsAutomaticWindowTabbing = false
     for window in NSApplication.shared.windows {
       window.applyLiquidGlass()
+      window.level = .floating
+      window.collectionBehavior.insert(.canJoinAllSpaces)
+      window.collectionBehavior.insert(.fullScreenAuxiliary)
     }
     NotificationCenter.default.addObserver(forName: NSWindow.didBecomeKeyNotification, object: nil, queue: .main) { note in
-      (note.object as? NSWindow)?.applyLiquidGlass()
+      guard let w = note.object as? NSWindow else { return }
+      w.applyLiquidGlass()
+      w.level = .floating
+      w.collectionBehavior.insert(.canJoinAllSpaces)
+      w.collectionBehavior.insert(.fullScreenAuxiliary)
     }
   }
 }
